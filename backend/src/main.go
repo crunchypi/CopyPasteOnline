0938125e54e43de49afa2ac7b19fa1d0fbb254b8
crunchypi <crunchypi@protonmail.com>
2020-08-29 23:02:00 +0200

    added example

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"CopyPasteOnline/ports/app"
+	"CopyPasteOnline/ports/mnemonics"
+	"CopyPasteOnline/ports/sqlite"
+)
 
+func main() {
+	// # Setup db manager
+	db, err := sqlite.New("./data.sqlite")
+	if err != nil {
+		panic("unable to setup db: " + err.Error())
+	}
+
+	// # Setup mnemonics handler Will panic on error.
+	mnemonics := mnemonics.New()
+
+	// # Stat server.
+	app.Start(db, mnemonics)
 }
