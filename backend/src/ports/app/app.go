4f62a888edf5c8a9c53dcf26d163ca05bdd867b7
crunchypi <crunchypi@protonmail.com>
2020-08-30 21:32:09 +0200

    integrated more configs & tls option

--- backend/src/ports/app/app.go
+++ backend/src/ports/app/app.go
@@ -4,6 +4,7 @@ import (
 	"copypaste-api/config"
 	"copypaste-api/ports/mnemonics"
 	"copypaste-api/ports/sqlite"
+	"crypto/tls"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,6 +13,19 @@ import (
 var (
 	ip   = config.IP
 	port = config.Port
+	// # Server IO time limitation.
+	readTimeout  = config.ReadTimeout
+	writeTimeout = config.WriteTimeout
+	// # Path to index.html. Disabled if empty.
+	staticFilePath = config.StaticFilePath
+)
+
+//
+var (
+	tlsEnabled         = config.TLSEnabled
+	serverName         = config.ServerName
+	certPublicKeyPath  = config.CertPublicKeyPath
+	certPrivateKeyPath = config.CertPrivateKeyPath
 )
 
 // handler serves as a bridge between the app and
@@ -21,50 +35,62 @@ type handler struct {
 	mnemonics *mnemonics.Poolhandler
 }
 
-// copyData handles incoming data and returns mnemonics.
-func (h *handler) copyData(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body) // # middleware err handled.
-
-	mnemonic, ok := h.mnemonics.DrawEnsured(h.db)
-	if !ok {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	err := h.db.CreateItem(mnemonic, body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(mnemonic))
-}
-
-// pasteData handles incoming mnemonics and returns stored data.
-func (h *handler) pasteData(w http.ResponseWriter, r *http.Request) {
-	body, _ := ioutil.ReadAll(r.Body) // # middleware err handled.
-
-	data, ok, err := h.db.ReadItemByMnemonic(string(body))
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(data.Data)
-}
-
 // setRoutes sets up routes for this app.
 func (h *handler) setRoutes() {
 	http.Handle("/copy", midDOS(midBodyErr(http.HandlerFunc(h.copyData))))
 	http.Handle("/paste", midDOS(midBodyErr(http.HandlerFunc(h.pasteData))))
+
+	if staticFilePath != "" {
+		http.Handle("/", midDOS(http.FileServer(http.Dir(staticFilePath))))
+	}
+}
+
+// tlsConfig is only called if tlsEnabled=true (pkg var)
+func tlsConfig() *tls.Config {
+	crt, err := ioutil.ReadFile(certPublicKeyPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	key, err := ioutil.ReadFile(certPrivateKeyPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cert, err := tls.X509KeyPair(crt, key)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ServerName:   serverName,
+	}
 }
 
 // Start starts the app.
 func Start(db *sqlite.SQLiteManager, mnemonics *mnemonics.Poolhandler) {
+	// # Enable interface to other ports of this microservice.
 	db.CreateItemTable()
 	handler := handler{db: db, mnemonics: mnemonics}
-
 	handler.setRoutes()
-	log.Println(http.ListenAndServe(ip+":"+port, nil))
+
+	// # Server configs.
+	server := http.Server{
+		Addr:         ip + ":" + port,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+	}
+	// # TLS on/off.
+	switch tlsEnabled {
+	case true:
+		server.TLSConfig = tlsConfig()
+		if err := server.ListenAndServeTLS("", ""); err != nil {
+			log.Fatal("shutdown:", err)
+		}
+	case false:
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatal("shutdown:", err)
+		}
+	}
 }
